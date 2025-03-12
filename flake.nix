e7f3e9d22bc4e868a4cc8332c2a0a67888272eef
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-03-13 02:55:51 +0800

    Fix colorscheme override

--- flake.nix
+++ flake.nix
@@ -12,13 +12,13 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       nixvim' = nixvim.legacyPackages.${system};
-      nvim = colorScheme: nixvim'.makeNixvimWithModule {
+      nvim = {colorScheme ? null}: nixvim'.makeNixvimWithModule {
         inherit pkgs;
         module = import ./config;
         extraSpecialArgs = { inherit colorScheme; };
       };
     in
       {
-        packages.${system}.default = pkgs.callPackage nvim { colorScheme = null; };
+        packages.${system}.default = pkgs.callPackage nvim { };
       };
 }
