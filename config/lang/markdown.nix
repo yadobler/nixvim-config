35bff5a8c89fcfcdbfeb07a30455577c734274ad
Yadobler <49450747+yadobler@users.noreply.github.com>
2026-01-31 18:43:42 +0800

    Replace markdown-preview with peek

--- config/lang/markdown.nix
+++ config/lang/markdown.nix
@@ -1,10 +1,41 @@
 { lib, pkgs, ... }:
 let
   helpers = lib.nixvim;
+  peek-built = pkgs.stdenv.mkDerivation rec {
+    pname = "peek-nvim";
+    version = "latest"; 
+
+    src = pkgs.fetchFromGitHub {
+      owner = "toppair";
+      repo = "peek.nvim";
+      rev = "5820d937d5414baea5f586dc2a3d912a74636e5b"; 
+      hash = "sha256-hGIPxHwTSXTHFJ3JiVATMjEmoFhZ87fWElj1AMPMbQU="; 
+    };
+
+    nativeBuildInputs = [ pkgs.deno ];
+
+    # Deno needs a writable home directory during the build
+    buildPhase = ''
+      export DENO_DIR=$TMPDIR/deno-dir
+      export DENO_NO_UPDATE_CHECK=1
+      deno task build:fast
+      '';
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r . $out
+      '';
+
+    # Adding 'outputHash' allows network access for Deno to fetch dependencies.
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = "sha256-/2m6YBXGGWbZWzq1mNmoGnndS3kP2AQFD+Wy8PGcrCE="; 
+  };
 in
   {
   extraPackages = with pkgs; [
     marksman
+    deno
   ];
 
   extraPlugins = [
@@ -34,7 +65,14 @@ in
 
   plugins = {
     markdown-preview = {
+      enable = false;
+    };
+    peek = {
       enable = true;
+      package = peek-built;
+      settings = {
+        app = "browser";
+      };
     };
     render-markdown = {
       enable = true;
@@ -105,7 +143,8 @@ in
     {
       mode = "n";
       key = "<leader>ccmp";
-      action = "<cmd>MarkdownPreviewToggle<cr>";
+      # action = "<cmd>MarkdownPreviewToggle<cr>";
+      action = "<cmd>PeekOpen<cr>";
       options = {
         silent = true;
         desc = "Toggle markdown preview";
